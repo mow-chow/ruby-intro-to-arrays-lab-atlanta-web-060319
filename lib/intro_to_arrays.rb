8e12bf956453f35a40f54d0822c6ee5438270061
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-13 01:48:31 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  nuarray = []
+  nuarray
+end
+
+def array_with_two_elements
+  nuarray = [1,2]
+  nuarray
+end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+ array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
